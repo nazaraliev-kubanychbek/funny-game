6fbe8e64f0d5c4803911ef9ea26ae5f9cfada6fb
nazaraliev-kubanychbek <kuba.duishobaevich@gmail.com>
2023-11-24 19:53:15 +0600

    initial

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+let btn = document.getElementById('btn');
+let text = document.getElementById('text');
+let wrapper = document.getElementById('wrapper');
+let btn2 = document.getElementById('btn2');
+
+text.style.display = 'none';
+
+btn.onclick = () => {
+    if (text.style.display == 'none') {
+        text.style.display = 'block';
+    } else{
+        text.style.display = 'none';
+    }
+}
+
+btn2.onclick = () =>{
+
+    wrapper.style.left = Math.round(Math.random() * (90 - 10) + 10)+'%';
+    wrapper.style.top = Math.round(Math.random() * (80 - 20) + 20)+'%';
+
+}
+
+
